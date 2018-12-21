dbc1b3015316d3332fe8b574eabeb3f25025f8c6
Anthony <antoni.p.bednarski@gmail.com>
2018-12-21 15:47:22 +0100

    Add sesion struct and method to create it.

---
+++ session.go
@@ -0,0 +1,27 @@
+//Copyright (c) 2018. Flying Jamnik Authors
+// license that can be found in the LICENSE file.
+
+package sessions
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Session struct {
+	ID                  uint64
+	UserID              int
+	CreatedAt, DeadLine time.Time
+}
+
+func RegisterSession(userid int) *Session {
+	id := rand.Uint64()
+
+	for id == 0 {
+		id = rand.Uint64()
+	}
+	createdat := time.Now().UTC()
+	deadline := time.Now().Add(7 * 24 * time.Hour).UTC()
+
+	return &Session{id, userid, createdat, deadline}
+}
